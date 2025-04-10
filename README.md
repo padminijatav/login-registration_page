5446e3a1d65ec5dd4eaeb6af3db3390466be4582
Padmini Jatav <pdmnjatav@gmail.com>
2025-04-10 18:38:11 +0530

    Add new project

---
+++ README.md
@@ -0,0 +1,20 @@
+# login-registration_page
+->This is a basic console-based user authentication system using C++. It allows users to:
+<br>
+-Register with a username and password.
+<BR>
+-Login using their credentials.
+<BR>
+-Retrieve their forgotten password by entering their username.
+<BR>
+->User credentials are stored in a plain text file (records.txt) for simplicity. This project demonstrates the use of:
+<BR>
+-File handling (ifstream, ofstream)
+<BR>
+-Conditional statements (if, switch)
+<BR>
+-Loops and functions
+<BR>
+-Basic string and user input/output operations
+<br>
+BY -Padmini Jatav 
---
+++ registration_and_login_page.cpp
@@ -0,0 +1,136 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+void login();
+void forgot();
+void registration();
+
+
+int main(){
+    int choice;
+
+    cout<<"\t\t\t __________________________________________________\n";
+    cout<<"\t\t\t|             Welcome to the login page            |\n";
+    cout<<"\t\t\t|-----------------   Menu   -----------------------|\n";
+    cout<<"\t\t\t|                                                  |\n";
+    cout<<"\t\t\t|  Press 1 to LOGIN                                |"<<endl;
+    cout<<"\t\t\t|  Press 2 to RESGISTER                            |"<<endl;
+    cout<<"\t\t\t|  Press 3 if you FORGOT YOUR PASSWORD             |"<<endl;
+    cout<<"\t\t\t|  Press 4 to EXIT                                 |\n";
+    cout<<"\t\t\t|__________________________________________________|\n";
+    
+    cout<<"\n\t\t\t Please Enter Your Choice : ";
+    cin>>choice;
+    cout<<endl;
+    
+    switch(choice){
+        case 1:
+            login();
+            break;
+        case 2:
+            registration();
+            break;
+        case 3:
+            forgot();
+            break;
+        case 4:
+            cout<<"\t\t\tTHANK YOU... \n";
+            cout<<"\t\t\tExiting!!!\n";
+        default:
+            system("cls");
+            cout<<"\t\t\tInvalid Choice Entered !!\n";
+            cout<<"\t\t\tSelect choice from Menu Given \n";
+    }
+    return 0;
+}
+
+void login(){
+    int count;
+    string userID;
+    string ID;
+    string password;
+    string pass;
+    system ("cls");
+
+    cout<<"\t\t\tEnter the username and password : "<<endl;
+    cout<<"\t\t\tUSERNAME : ";
+    cin>>userID;
+    cout<<"\t\t\tPASSWORD : ";
+    cin>>password;
+    ifstream input("records.txt");
+    while(input>>ID>>pass){
+        if(ID==userID && pass==password){
+            count=1;
+            system("cls");
+        }
+    }
+    input.close();
+    
+    if(count==1){
+        cout<<userID <<"\n your LOGIN is successful \n Thnaks for logging in ! \n ";
+        main();
+    }else{
+        cout<<"\n LOGIN ERROR \n Please check your username and password \n";
+        main();
+    }
+}
+
+void registration(){
+    string ruserID;
+    string rpassword;
+    string rID;
+    string rpass;
+    system("cls");
+    cout<<"\t\t\tEnter the username : ";
+    cin>>ruserID;
+    cout<<"\t\t\tEnter the password : ";
+    cin>>rpassword;
+
+    ofstream f1("records.txt",ios::app);
+    f1<<ruserID<<' '<<rpassword<<endl;
+    system("cls");
+    cout<<"\n\t\t\t Registration is successful ! \n";
+}
+
+void forgot(){
+    int option;
+    system("cls");
+    cout<<"\t\t\tYou forget the passwod? No worries \n";
+    cout<<"Press 1 to search your id by username "<<endl;
+    cout<<"Press 2 to go back to the main menu  "<<endl;
+    cout<<"\t\t\tEnter your choice : ";
+    cin>>option;
+    switch(option){
+        case 1:{
+            int count =0;
+            string suserID,sID,sPass;
+            cout<<"\n\t\t\tEnter the username which you remembered : ";
+            cin>>suserID;
+
+            ifstream f2("records.txt");
+            while(f2>>sID>>sPass){
+                if(sID==suserID){
+                    count =1;
+                }
+            }
+            f2.close();
+            if(count==1){
+                cout<<"\n\n Your account is found ! \n";
+                cout<<"\n\n Your password is : "<<sPass<<endl;
+            }else{
+                cout<<"\n\t Sorry !! your account is not found !!\n";
+            }
+        }
+            break;
+        case 2:{
+            return;
+        }
+            break;
+        default:
+            cout<<"\t\t\t Wrong choice ! please try again "<<endl;
+            forgot();
+
+    }
+}
